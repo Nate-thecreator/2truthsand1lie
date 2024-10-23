9029db08a1d10adfe3f3aaf3c521d6cee9220c4c
TarekeEPS <ntareke@eastsideprep.org>
2024-10-23 09:53:05 -0700

    2truthsandalie

--- truth.js
+++ truth.js
@@ -1,17 +1,17 @@
 function trivia1() {
     let triviaAnswer = document.getElementById("trivia1-answer");
     // get values from form
-    let chocolateSelected = document.getElementById("sibling").checked;
-    let tunaSelected = document.getElementById("basketball").checked;
-    let honeySelected = document.getElementById("ultimate").checked;
+    let siblingSelected = document.getElementById("sibling").checked;
+    let basketballSelected = document.getElementById("basketball").checked;
+    let ultimateSelected = document.getElementById("ultimate").checked;
     // give result based off of what they selected
     if (siblingSelected) {
-        triviaAnswer.innerHTML = fname + ", you are right! ";
+        triviaAnswer.innerHTML =  " you are wrong. ";
     }
     else if (basketballSelected) {
-        triviaAnswer.innerHTML = fname + ", you are wrong. ";
+        triviaAnswer.innerHTML = " you are wrong. ";
     }
     else if (ultimateSelected) {
-        triviaAnswer.innerHTML = fname + ", you are wrong.";
+        triviaAnswer.innerHTML = " you are right!";
     }
 }
